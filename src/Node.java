bcb86e784eda6b4c60f6e4d4d105921c804faa43
Jason Chan <13chanja@gmail.com>
2021-02-07 02:25:43 -0800

    A* misplaced Tiles Complete

--- src/Node.java
+++ src/Node.java
@@ -1,18 +1,49 @@
 import java.util.ArrayList;
+import java.util.Objects;
 
-public class Node {
-    Node parent;
-
-    boolean visited;
-
-    ArrayList<Integer> state;
-
+public class Node implements Comparable<Node>{
+    Node parent;    //parent node, doesnt actually hold state information unless specified
+    ArrayList<Integer> state;   //actual state of the board
     ArrayList<Node> children;//should only go up to 4
+    boolean visited;
+    int weight;
+    int depth;
 
+    //Node constructor for uniform cost search
     Node(Node p, ArrayList<Integer> a){
         parent = p;
         state = a;
         children = null;
         visited = false;
+        weight = 999;
+        depth = 0;
+    }
+
+    //Node constructor for A*
+    Node(Node p, ArrayList<Integer> a, int w){
+        parent = p;
+        state = a;
+        children = null;
+        visited = false;
+        weight = w;
+        depth = 0;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    //Override for A* priority queue to order based on weight
+    @Override
+    public int compareTo(Node node){
+        if(this.getWeight() > node.getWeight()){
+            return 1;
+        }
+        else if(this.getWeight() < node.getWeight()){
+            return -1;
+        }
+        else{
+            return 0;
+        }
     }
 }
--- src/eightPuzzle.java
+++ src/eightPuzzle.java
@@ -2,14 +2,156 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.PriorityQueue;
 
 
 public class eightPuzzle {
 
+    //I think this is 31 for an 8 puzzle, change this if n changes
+    final static int DIAMETER = 31;
+    //N is the dimensions of the nxn matrix
+    final static int N = 3;
+
+
+    public static void aStarMisplacedTile(ArrayList<Integer> intial, ArrayList<Integer> solution, int n){
+        //int depth = 0;
+        int LIMIT = DIAMETER;
+        //Assign the initial state to the root node
+        //We'll track tree nodes which will contain a board states in each node
+        ArrayList<Integer> initState = new ArrayList<>(intial);
+        Node root = new Node(null, initState, misplacedTileCount(initState, solution, n));
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        //Priority Queue Test Demo
+//        Node a = new Node(null, initState, 5);
+//        Node b = new Node(null, initState, 7);
+//        Node c = new Node(null, initState, 4);
+//        pq.add(a);
+//        pq.add(b);
+//        pq.add(c);
+//        while (!pq.isEmpty()){
+//            System.out.println(pq.remove().getWeight());
+//        }
+
+        Node traverse = root;
+        pq.add(traverse);
+
+        while(!pq.isEmpty()){
+            pq.remove();//pop traverse, marks as visited
+            if(traverse.state.equals(solution)){
+                break; //found solution so return solved Node
+            }
+            if(traverse.depth < LIMIT){
+                misplacedTileExpandNode(traverse,solution,n);
+                for(int i = 0; i < traverse.children.size(); i++){
+                    pq.add(traverse.children.get(i));
+                    traverse.children.get(i).depth = traverse.depth + 1;
+//                    System.out.println(traverse.children.get(i).getWeight());
+                }
+//                System.out.println();
+            }
+            traverse = pq.peek();
+//            System.out.println("current front:" + pq.peek().getWeight());
+//            printState(pq.peek().state, n);
+        }
+
+        //show solution...
+//        if(pq.isEmpty()){
+//            System.out.println("No Solution is possible.");
+//        }
+//        else{
+//            while(traverse != null){
+//                printState(traverse.state,n);
+//                System.out.println();
+//                traverse = traverse.parent;
+//            }
+//        }
+    }
+
+    public static int misplacedTileCount(ArrayList<Integer> state, ArrayList<Integer> solution, int n){
+        int count = 0;
+        int tot = n*n;
+        for(int i = 0; i < tot; i++){
+            //System.out.println(state.get(i) + " " + solution.get(i));
+            if(state.get(i) != solution.get(i)){
+                count++;
+            }
+        }
+        //System.out.println(count);
+        return count;
+    }
+
+    //same thing as expand node except we now calculate the weight
+    public static void misplacedTileExpandNode(Node node, ArrayList<Integer> solution, int n){
+
+        //This save each swap state
+        ArrayList<Integer> left = expandLeft(node.state,n);
+        ArrayList<Integer> right = expandRight(node.state,n);
+        ArrayList<Integer> top = expandTop(node.state,n);
+        ArrayList<Integer> bottom = expandBottom(node.state,n);
+        //but not all may be valid swaps in the puzzle so
+        //we'll do a check and add valid swaps to the list of child nodes
+
+        //list of expansions
+        node.children = new ArrayList<>();
+        //swapped left number and add to list if valid
+        if(left != null){
+            if(node.parent == null) {
+                node.children.add(new Node(node, left, misplacedTileCount(left,solution,n)));
+            }
+            else{
+                if(!node.parent.state.equals(left)){//check for repeated states
+                    node.children.add(new Node(node, left, misplacedTileCount(left,solution,n)));
+                }
+            }
+        }
+        //swapped right number and add to list if valid
+        if(right != null){
+            if(node.parent == null) {
+                node.children.add(new Node(node, right, misplacedTileCount(right,solution,n)));
+            }
+            else{
+                if(!node.parent.state.equals(right)){//check for repeated states
+                    node.children.add(new Node(node, right, misplacedTileCount(right,solution,n)));
+                }
+            }
+        }
+        //swapped top number and add to list if valid
+        if(top != null){
+            if(node.parent == null) {
+                node.children.add(new Node(node, top, misplacedTileCount(top,solution,n)));
+            }
+            else{
+                if(!node.parent.state.equals(top)){//check for repeated states
+                    node.children.add(new Node(node, top, misplacedTileCount(top,solution,n)));
+                }
+            }
+        }
+        //swapped bottom number and add to list if valid
+        if(bottom != null){
+            if(node.parent == null) {
+                node.children.add(new Node(node, bottom, misplacedTileCount(bottom,solution,n)));
+            }
+            else{
+                if(!node.parent.state.equals(bottom)){//check for repeated states
+                    node.children.add(new Node(node, bottom, misplacedTileCount(bottom,solution,n)));
+                }
+            }
+        }
+    }
+
+
+
+
+
     //This function does the BFS traversal
+    //n: is the size of the matrix, nxn
+    //initial: initial state
+    //solution: solution state
     public static void uniformCostSearch(ArrayList<Integer> intial, ArrayList<Integer> solution, int n){
         int depth = 0;
-        int LIMIT = 31;//diameter I think is 31
+        int LIMIT = DIAMETER;
 
         //Assign the initial state to the root node
         //We'll track tree nodes which will contain a board states in each node
@@ -25,14 +167,20 @@ public class eightPuzzle {
 
         parents.add(root);
 
+        //count will keep track of node created. ONLY for this type of search.
+        //we start with 1 nodes for 1 parent and the children will be made
+        int count = 1;
+
         //first iteration outside loop
         Node traverse = parents.get(0);
         expandNode(parents.get(0), n);
         ArrayList<Node> childs = new ArrayList<>();
 
-        //...still part of first iteration
+        //...still part of first iteration, creating children
         for(int j = 0; j < traverse.children.size(); j++){
             childs.add(traverse.children.get(j));
+            //each child made is a new node added, so count+1
+            count++;
         }
 
         //Okay now this is the loop we will do BFS if the root node isn't already the solution
@@ -40,16 +188,17 @@ public class eightPuzzle {
             depth++;//tracks the depth of the tree
             parents = childs;
             childs = new ArrayList<>();
-            for(int i = 0; i < parents.size(); i++){    //for each parent check with solution and expand
+            for(int i = 0; i < parents.size(); i++){    //for each parent check with solution or else expand
                 traverse = parents.get(i);
                 //System.out.println();//DEBUG
                 //printState(traverse.state,n);//DEBUG
                 if(traverse.state.equals(solution)){
-                    break;
+                    break;  //SOLUTION FOUND BREAK
                 }
                 expandNode(traverse, n);
                 for(int j = 0; j < traverse.children.size(); j++){  //add all the expansions to a childs list
                     childs.add(traverse.children.get(j));
+                    count++;
                 }
             }
             if(depth > LIMIT){
@@ -57,16 +206,21 @@ public class eightPuzzle {
             }
         }
 
-        //output solution (probably should be in a separate function...)
-        while (traverse.parent != null){
-            System.out.println();
-            printState(traverse.state,n);
-            traverse = traverse.parent;
-        }
-        System.out.println();
-        printState(traverse.state,n);
-
-        System.out.println("Solved at depth: " + depth);
+//        //output solution (probably should be in a separate function...)
+//        if(depth>LIMIT){
+//            System.out.println("FAILED");
+//        }
+//        else{
+//            while (traverse.parent != null){
+//                System.out.println();
+//                printState(traverse.state,n);
+//                traverse = traverse.parent;
+//            }
+//            System.out.println();
+//            printState(traverse.state,n);
+//
+//            System.out.println("Solved at depth: " + depth + ".\nCreated " + count + " nodes total.");
+//        }
     }
 
     //expandNode()
@@ -199,7 +353,13 @@ public class eightPuzzle {
     public static void printState(ArrayList<Integer> ar, int n){
         int sqrd = n*n;
         for (int i = 0; i < sqrd; i++){
-            System.out.print(ar.get(i) + " ");
+            if(ar.get(i) == 0){
+                //print zero or empty
+                System.out.print(" " + " ");
+            }
+            else{
+                System.out.print(ar.get(i) + " ");
+            }
             if((i+1)%n==0){
                 System.out.println();
             }
@@ -208,9 +368,6 @@ public class eightPuzzle {
 
 
     public static void main(String[] args){
-
-        //N is the dimensions of the nxn matrix
-        int N = 3;
 
         //Initial State
         ArrayList<Integer> dep0 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,0));
@@ -223,6 +380,10 @@ public class eightPuzzle {
         ArrayList<Integer> dep20 = new ArrayList<>(Arrays.asList(7,1,2,4,8,5,6,3,0));
         ArrayList<Integer> dep24 = new ArrayList<>(Arrays.asList(0,7,2,4,6,1,3,5,8));
 
+        //List all possible solutions
+        ArrayList<ArrayList<Integer>> possibleSolutions = new ArrayList<>(
+                Arrays.asList(dep0, dep1, dep2, dep4, dep8, dep12, dep16, dep20, dep24));
+
         //Solution State
         //for an 8 puzzle: (1,2,3,4,5,6,7,8,0)
         //1,2,3
@@ -231,9 +392,26 @@ public class eightPuzzle {
         //This is the solution state
         ArrayList<Integer> sol = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,0));
 
+        //ArrayList<Integer> unsolvable = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,8,7,0));
 
+        long start;
+        long end;
+        double time;
+
+        start = System.nanoTime();
         //pass the inital and solution state through
-        uniformCostSearch(dep24, sol, N);
+        uniformCostSearch(dep24,sol,N);
+        end = System.nanoTime();
+        time = ((end - start) / 1e9);
+        System.out.println("Time: " + time + " seconds");
+
+
+
+        start = System.nanoTime();
+        aStarMisplacedTile(dep24,sol,N);
+        end = System.nanoTime();
+        time = ((end - start) / 1e9);
+        System.out.println("Time: " + time + " seconds");
 
         return;
     }
